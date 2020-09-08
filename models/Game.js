d842587b35a5a13359969941755e1c485f5a93e4
mustaque <hussainmustaque98407@gmail.com>
2020-09-08 14:28:11 +0530

    add game model

---
+++ models/Game.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+const { db } = require("../config/db");
+
+const Game = db.define("Game", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  displayPic: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  trailerLink: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+Game.sync();
+
+module.exports = Game;
