caed59e7bd96507ed1145cdc3b18bf99be7bcc41
mustaque <hussainmustaque98407@gmail.com>
2020-11-12 22:05:50 +0530

    add game types in redux

---
+++ client/src/actions/types.js
@@ -0,0 +1,4 @@
+export const ADD_GAME = 'ADD_GAME';
+export const SHOW_ALL_GAMES = 'SHOW_ALL_GAMES';
+export const EDIT_GAME = 'EDIT_GAME';
+export const DELETE_GAME = 'DELETE_GAME';
--- client/src/reducers/game.js
+++ client/src/reducers/game.js
@@ -1,3 +1,10 @@
+import {
+  ADD_GAME,
+  SHOW_ALL_GAMES,
+  EDIT_GAME,
+  DELETE_GAME,
+} from '../actions/types';
+
 const initialState = {
   games: [],
   loading: false,
@@ -5,9 +12,14 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case 'ADD_GAME':
+    case ADD_GAME:
       return state;
-
+    case SHOW_ALL_GAMES:
+      return state;
+    case EDIT_GAME:
+      return state;
+    case DELETE_GAME:
+      return state;
     default:
       return state;
   }
