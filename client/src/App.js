ebad8b011df1d7f631923e7886a191eb52370b1e
mustaque <hussainmustaque98407@gmail.com>
2020-11-14 13:12:59 +0530

    implement redux store and react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavigationBar from './components/layout/NavigationBar';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import './App.css';
 
 function App() {
-  return <NavigationBar />;
+  return (
+    <BrowserRouter>
+      <Fragment>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </Fragment>
+    </BrowserRouter>
+  );
 }
 
 export default App;
