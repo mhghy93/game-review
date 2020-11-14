aa7dfcdca392840f23b2d03a0f9f739b8df6d552
mustaque <hussainmustaque98407@gmail.com>
2020-11-14 14:03:56 +0530

    navbar cleanup

--- client/src/components/layout/NavigationBar.js
+++ client/src/components/layout/NavigationBar.js
@@ -3,14 +3,19 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 const NavigationBar = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="light"
+      variant="light"
+      sticky="top"
+    >
+      <Navbar.Brand href="#home">Game Reviews</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+          <Nav.Link href="#features">About</Nav.Link>
+          <NavDropdown title="Categories" id="collasible-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
               Another action
@@ -23,9 +28,9 @@ const NavigationBar = () => {
           </NavDropdown>
         </Nav>
         <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link href="#deets">Sign up</Nav.Link>
           <Nav.Link eventKey={2} href="#memes">
-            Dank memes
+            Log In
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
