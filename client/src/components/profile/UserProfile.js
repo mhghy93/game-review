33de0839d20d92461f3a056d589188c69d909a52
mustaque <hussainmustaque98407@gmail.com>
2020-12-18 01:06:07 +0530

    user profile init

--- client/src/components/profile/UserProfile.js
+++ client/src/components/profile/UserProfile.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Card, Row, Col, Button } from 'react-bootstrap';
 import { loadUser } from '../../actions/auth';
 
 const UserProfile = ({ auth: { user }, loadUser }) => {
@@ -8,7 +9,28 @@ const UserProfile = ({ auth: { user }, loadUser }) => {
     loadUser();
   }, [loadUser]);
 
-  return <div>hello {user.username}</div>;
+  return (
+    <Fragment>
+      <Card className="shadow bg-white rounded p-5">
+        <Row>
+          <Col xs={6} lg={4}>
+            <Card.Img
+              src="https://res.cloudinary.com/mhghy93/image/upload/v1578161259/blank-profile-picture-973460_1280_xf4t0m.png"
+              variant="top"
+            />
+          </Col>
+          <Col xs={6} lg={4}>
+            <h5>{user.username}</h5>
+            <Button variant="success">Edit Profile</Button>
+            <p>Member since {new Date(user.createdAt).toLocaleDateString()}</p>
+          </Col>
+          <Col xs={6} lg={4}>
+            Reviews
+          </Col>
+        </Row>
+      </Card>
+    </Fragment>
+  );
 };
 
 UserProfile.propTypes = {
