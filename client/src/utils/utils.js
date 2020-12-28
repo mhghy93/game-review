63271d25aefa5d5a68a1140000a14e0b2359e9b8
mustaque <hussainmustaque98407@gmail.com>
2020-12-28 23:52:21 +0530

    util function bug

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -5,8 +5,9 @@ export function uniqueArray(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].includes(',')) {
       temp = arr[i].split(',');
-      uniqueArr.push(temp[0]);
-      uniqueArr.push(temp[1]);
+      for (let j = 0; j < temp.length; j++) {
+        uniqueArr.push(temp[j]);
+      }
     } else {
       uniqueArr.push(arr[i]);
     }
