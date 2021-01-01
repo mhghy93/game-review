9f5794846f86a0778babe1cea24c887c752e0ddd
mustaque <hussainmustaque98407@gmail.com>
2021-01-01 13:15:11 +0530

    ui improvements

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Form, Button, Card, Row, Col } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { setAlert } from '../../actions/alert';
 import { userRegister } from '../../actions/auth';
@@ -49,120 +49,110 @@ const Register = ({
 
   return (
     <Fragment>
-      <Row className="justify-content-center px-5 pt-2 pb-5">
-        <Col lg={8}>
-          <Card className="shadow bg-white rounded">
-            <div className="bg-secondary text-white">
-              {' '}
-              <h5 className="text-center mt-3">Register</h5>
-            </div>
-            <Card.Body>
-              <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="formBasicFirstName">
-                  <Form.Label>First Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter firstname"
-                    name="firstname"
-                    value={firstname}
-                    onChange={onChange}
-                    required
-                  />
-                  <Form.Text className="text-muted">
-                    Please enter your firstname
-                  </Form.Text>
-                </Form.Group>
+      <div className="mb-5 bg-white shadow rounded p-5">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter firstname"
+              name="firstname"
+              value={firstname}
+              onChange={onChange}
+              required
+            />
+            <Form.Text className="text-muted">
+              Please enter your firstname
+            </Form.Text>
+          </Form.Group>
 
-                <Form.Group controlId="formBasicLastName">
-                  <Form.Label>Last Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter lastname"
-                    name="lastname"
-                    value={lastname}
-                    onChange={onChange}
-                    required
-                  />
-                  <Form.Text className="text-muted">
-                    Please enter your lastname
-                  </Form.Text>
-                </Form.Group>
+          <Form.Group controlId="formBasicLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter lastname"
+              name="lastname"
+              value={lastname}
+              onChange={onChange}
+              required
+            />
+            <Form.Text className="text-muted">
+              Please enter your lastname
+            </Form.Text>
+          </Form.Group>
 
-                <Form.Group controlId="formBasicUserName">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter username"
-                    name="username"
-                    value={username}
-                    onChange={onChange}
-                    required
-                  />
-                  <Form.Text className="text-muted">
-                    Please enter your username
-                  </Form.Text>
-                </Form.Group>
+          <Form.Group controlId="formBasicUserName">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              name="username"
+              value={username}
+              onChange={onChange}
+              required
+            />
+            <Form.Text className="text-muted">
+              Please enter your username
+            </Form.Text>
+          </Form.Group>
 
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    name="email"
-                    value={email}
-                    onChange={onChange}
-                    required
-                  />
-                  <Form.Text className="text-muted">
-                    Please enter a valid email.
-                  </Form.Text>
-                </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              value={email}
+              onChange={onChange}
+              required
+            />
+            <Form.Text className="text-muted">
+              Please enter a valid email.
+            </Form.Text>
+          </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={password}
-                    onChange={onChange}
-                    required
-                    minLength="6"
-                  />
-                  <Form.Text className="text-muted">
-                    Minimum password length should be 6.
-                  </Form.Text>
-                </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              required
+              minLength="6"
+            />
+            <Form.Text className="text-muted">
+              Minimum password length should be 6.
+            </Form.Text>
+          </Form.Group>
 
-                <Form.Group controlId="formBasicConfirmPassword">
-                  <Form.Label>Confirm Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Confirm Password"
-                    name="confirmPassword"
-                    value={confirmPassword}
-                    onChange={onChange}
-                    required
-                    minLength="6"
-                  />
-                  <Form.Text className="text-muted">
-                    Minimum password length should be 6.
-                  </Form.Text>
-                </Form.Group>
+          <Form.Group controlId="formBasicConfirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={onChange}
+              required
+              minLength="6"
+            />
+            <Form.Text className="text-muted">
+              Minimum password length should be 6.
+            </Form.Text>
+          </Form.Group>
 
-                <Button variant="success" type="submit" block>
-                  Register
-                </Button>
-              </Form>
-              <LinkContainer to="/login">
-                <Button className="mt-3" variant="link" block>
-                  Have an account?
-                </Button>
-              </LinkContainer>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+          <Button variant="success" type="submit" block>
+            Register
+          </Button>
+        </Form>
+        <LinkContainer to="/login">
+          <Button className="mt-3" variant="link" block>
+            Have an account?
+          </Button>
+        </LinkContainer>
+      </div>
     </Fragment>
   );
 };
--- client/src/components/games/GameDetail.js
+++ client/src/components/games/GameDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import ReactPlayer from 'react-player';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -8,40 +8,47 @@ import { showGameDetail } from '../../actions/game';
 import Reviews from '../reviews/Reviews';
 import GamePlatform from './GamePlatform';
 import GameCategory from './GameCategory';
+import Loading from '../layout/Loading';
 
-const GameDetail = ({ showGameDetail, game: { game }, match }) => {
+const GameDetail = ({ showGameDetail, game: { game, loading }, match }) => {
   useEffect(() => {
     showGameDetail(match.params.id);
   }, [showGameDetail, match.params.id]);
 
   return (
-    <div className="mt-3 mr-5 ml-5 mb-5">
-      <LinkContainer to="/">
-        <Button variant="light">Back</Button>
-      </LinkContainer>
-      <h3 className="text-center mb-3">{game.title}</h3>
-      <Image src={game.displayPic} thumbnail />
-      <div className="player-wrapper mt-3 mb-3">
-        <ReactPlayer
-          className="react-player"
-          url={game.trailerLink}
-          width="100%"
-          height="100%"
-          controls={true}
-        />
-      </div>
-      <p className="mt-3">{game.description}</p>
-      <GameCategory category={game.category} />
-      <GamePlatform platform={game.platform} />
-      <div className="mt-5">
-        <LinkContainer to={`/game/review/${game.id}/add`}>
-          <Button variant="warning">
-            <i className="fas fa-plus"></i> Add Review
-          </Button>
-        </LinkContainer>
-      </div>
-      <Reviews id={match.params.id} />
-    </div>
+    <Fragment>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="mt-3 mr-5 ml-5 mb-5">
+          <LinkContainer to="/">
+            <Button variant="light">Back</Button>
+          </LinkContainer>
+          <h3 className="text-center mb-3">{game.title}</h3>
+          <Image src={game.displayPic} thumbnail />
+          <div className="player-wrapper mt-3 mb-3">
+            <ReactPlayer
+              className="react-player"
+              url={game.trailerLink}
+              width="100%"
+              height="100%"
+              controls={true}
+            />
+          </div>
+          <p className="mt-3">{game.description}</p>
+          <GameCategory category={game.category} />
+          <GamePlatform platform={game.platform} />
+          <div className="mt-5">
+            <LinkContainer to={`/game/review/${game.id}/add`}>
+              <Button variant="warning">
+                <i className="fas fa-plus"></i> Add Review
+              </Button>
+            </LinkContainer>
+          </div>
+          <Reviews id={match.params.id} />
+        </div>
+      )}
+    </Fragment>
   );
 };
 
